480521001cc1d107c5b8a738f0cd36e0b5842b1d
BerezaOlena <bereza.olena@gmail.com>
2023-10-19 21:50:10 +0300

    hw browser size mode

--- conftest.py
+++ conftest.py
@@ -11,7 +11,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_mode', action='store', default="headless",
                      help="By default is headless mode, but you can set --browser_mode='qui'")
     parser.addoption('--browser_window_size', action='store', default="norma",
-                     help="By default is standart mode, but you can set --browser_window_size='norma'")
+                     help="By default is standart mode, but you can set --browser_window_size='max'")
 
 @pytest.fixture()
 def browser(request):
